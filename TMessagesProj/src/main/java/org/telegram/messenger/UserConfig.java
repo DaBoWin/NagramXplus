3dec81f2cbae1626ad4093e7f71b1edb2fae0243
robert <robert@robertdeMac-mini-2.local>
2026-02-20 23:42:08 +0800

    Fix account limit: Support 30 accounts with dynamic instance creation
    - Change MAX_ACCOUNT_COUNT from 10 to 30 in both C++ and Java
    - Replace hardcoded switch-case with dynamic instance creation in ConnectionsManager
    - Fix crash when accessing accounts beyond index 9 due to uninitialized jniEnv pointers

--- TMessagesProj/src/main/java/org/telegram/messenger/UserConfig.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/UserConfig.java
@@ -1,4 +1,4 @@
-/*
+hai/*
  * This is the source code of Telegram for Android v. 5.x.x.
  * It is licensed under GNU GPL v. 2 or later.
  * You should have received a copy of the license in this archive (see LICENSE).
@@ -26,8 +26,8 @@ import tw.nekomimi.nekogram.NekoConfig;
 public class UserConfig extends BaseController {
 
     public static int selectedAccount;
-    public final static int MAX_ACCOUNT_DEFAULT_COUNT = 100;
-    public final static int MAX_ACCOUNT_COUNT = 100;
+    public final static int MAX_ACCOUNT_DEFAULT_COUNT = 30;
+    public final static int MAX_ACCOUNT_COUNT = 30;
 
     private final Object sync = new Object();
     private volatile boolean configLoaded;
